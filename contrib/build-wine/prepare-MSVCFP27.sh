6f33523b3ef6ce67273808a0e4262d5922fe7113
motty <admin@fujicoin.org>
2017-07-10 09:45:44 +0900

    fix MSVCFP27.sh

--- contrib/build-wine/prepare-MSVCFP27.sh
+++ contrib/build-wine/prepare-MSVCFP27.sh
@@ -5,5 +5,5 @@ MSVCFP27=https://electrum.fujicoin.org/VCForPython27.msi
 
 # Install Microsoft Visual C++ Compiler for Python 2.7
 wget -O VCForPython27.msi --no-check-certificate "$MSVCFP27"
-wine msiexec /i VCForPython27.msi
+wine msiexec /i VCForPython27.msi ALLUSERS=1
 
